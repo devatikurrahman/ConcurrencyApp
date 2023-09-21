8c1d83792cc1e0c9a8843ff5992d5430ae130d8a
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-22 01:38:20 +0600

    ConcurrencyApp created

---
+++ ConcurrencyApp/ConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAppApp.swift
+//  ConcurrencyApp
+//
+//  Created by Atikur Rahman on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
