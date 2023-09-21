884d83e1097eaee6c4479623509882145875efa7
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-22 02:30:25 +0600

    Example with Task

--- ConcurrencyApp/ContentView.swift
+++ ConcurrencyApp/ContentView.swift
@@ -8,24 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var site = "https://"
     @State private var sourceCode = ""
     
     var body: some View {
+        VStack {
+            HStack {
+                TextField("Website address", text: $site)
+                    .textFieldStyle(.roundedBorder)
+                Button("Go") {
+                    print("Button clicked: Synchronous code start")
+                    Task {
+                        print("Tab button")
+                        await fetchSource()
+                        print("End data processing")
+                    }
+                    print("Button clicked: Synchronous code end")
+                }
+            }
+            .padding()
+        }
         ScrollView {
             Text(sourceCode)
         }
-        .task {
-            await fetchSource()
-        }
+        .foregroundColor(.black)
+        
     }
     
     func fetchSource() async {
         do {
-            let url = URL(string: "https://apple.com")!
+            let url = URL(string: site)!
             let (data, _) = try await URLSession.shared.data(from: url)
             sourceCode = String(decoding: data, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
+            print("sourceCode: \(sourceCode)")
         } catch {
-            sourceCode = "Failed to fetch apple.com"
+            sourceCode = "Failed to fetch \(site)"
         }
     }
 }
