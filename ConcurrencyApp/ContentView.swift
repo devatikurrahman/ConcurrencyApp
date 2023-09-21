67ed38eb1a7dbd8c165cb028ae96c7d0e79a5726
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-22 02:12:23 +0600

    Async with .task() modifier

--- ConcurrencyApp/ContentView.swift
+++ ConcurrencyApp/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sourceCode = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            Text(sourceCode)
         }
-        .padding()
+        .task {
+            await fetchSource()
+        }
+    }
+    
+    func fetchSource() async {
+        do {
+            let url = URL(string: "https://apple.com")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            sourceCode = String(decoding: data, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
+        } catch {
+            sourceCode = "Failed to fetch apple.com"
+        }
     }
 }
 
